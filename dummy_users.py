cdcc4c7682cdd74c5f4bac1ae9aa60dbac1bc6c9
Araz Nabi <araznabi@gmail.com>
2023-09-26 13:05:31 +0200

    fixed columns

--- dummy_users.py
+++ dummy_users.py
@@ -14,7 +14,8 @@ user_names = {'Name': [
     'Samuel', 'Alexander', 'Henry', 'Daniel', 'Tom', 'Yaya', 'Joseph','Hardy'
 ]}
 
-user_answers = pd.DataFrame(columns=['Name', 'Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6'])
+user_answers = pd.DataFrame(columns=['Name', 'Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6', 'Question 7', 
+                                     'Question 8', 'Question 9', 'Question 10', 'Question 11', 'Question 12', 'Question 13', 'Question 14', 'Question 15','Question 16', ])
 
 
 def create_user():
