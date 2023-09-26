1d2c35284a2090dce38385ac333fc1e4234dbea2
Araz Nabi <araznabi@gmail.com>
2023-09-26 13:40:20 +0200

    with dummy data

--- dummy_users.py
+++ dummy_users.py
@@ -5,13 +5,13 @@ user_names = {'Name': [
     'Olivia', 'Liam', 'Emma', 'Noah', 'Ava', 'Sophia', 'Isabella', 'Mia', 'Jackson', 'Aiden',
     'Lucas', 'Ethan', 'Patricia', 'Harper', 'Henry', 'Khaqan', 'Lily', 'Grace', 'Samuel', 'Benjamin',
     'Henry', 'Tell', 'James', 'Oliver', 'Elijah', 'Mia', 'Sofia', 'Avery', 'Scarlett', 'Madison',
-    'Ava', 'Eleanor', 'Cruz', 'Elizabeth', 'Evelyn', 'Victoria', 'Amelia', 'Emily', 'Sebastian', 'Matthew',
-    'Alexander', 'Daniel', 'Joseph', 'David', 'Michael', 'Yat', 'Chloe', 'Zoe', 'Grace', 'Hannah',
-    'Andrew', 'William', 'Daniel', 'Anthony', 'Christopher', 'Jackson', 'Josephine', 'Natalie', 'Addison', 'Beckham',
+    'Avatar', 'Eleanor', 'Cruz', 'Elizabeth', 'Evelyn', 'Victoria', 'Amelia', 'Emily', 'Sebastian', 'Matthew',
+    'Alexander', 'Danny', 'Joseph', 'David', 'Michael', 'Yat', 'Chloe', 'Zoe', 'Grace', 'Hannah',
+    'Andrew', 'William', 'Felli', 'Anthony', 'Christopher', 'Jackson', 'Josephine', 'Natalie', 'Addison', 'Beckham',
     'Peter', 'Samantha', 'Araz', 'Nora', 'Ella', 'Scarlett', 'Grace', 'Evelyn', 'Aubrey', 'Camila', 'Thor',
     'Daniela', 'Sophia', 'Evelyn', 'Charlotte', 'Mia', 'Simon', 'Abigail', 'Emily', 'Elizabeth', 'Aria', 'Wayne',
     'Tawan', 'Scarlett', 'Amelia', 'Ali', 'Hussein', 'Penelope', 'Avery', 'Claire', 'Laila', 'Bond', 'Button',
-    'Samuel', 'Alexander', 'Henry', 'Daniel', 'Tom', 'Yaya', 'Joseph','Hardy'
+    'Samuel', 'Alexander', 'Holly', 'Daniel', 'Tom', 'Yaya', 'Joseph','Hardy'
 ]}
 
 user_answers = pd.DataFrame(columns=['Name', 'Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6', 'Question 7', 
@@ -43,4 +43,13 @@ def create_user():
 
 
 create_user()
-print(user_answers)
+
+mean_columns = ['Feeling', 'Perceiving', 'Introversion', 'Sensing', 'Thinking', 'Judging', 'Extroversion', 'Intuition']
+mean_values = user_answers.iloc[:, 1::2].mean(axis=1)
+user_feelings = pd.DataFrame({'Name': user_answers['Name']})
+for col_name, mean_col in zip(mean_columns, range(0, 16, 2)):
+    user_feelings[col_name] = mean_values
+    mean_values = user_answers.iloc[:, mean_col + 2::2].mean(axis=1)
+
+print(user_feelings)
+user_feelings.to_csv("dummy_data.csv")
