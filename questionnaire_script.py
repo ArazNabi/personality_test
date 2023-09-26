db975454b6cc510c47c822de9ea964530b59c712
Simon Janlöv <simon.janlov@gmail.com>
2023-09-26 12:17:34 +0200

    Worked on questionnaire_script. It can now read in the questions and print them one by one in the terminal

--- questionnaire_script.py
+++ questionnaire_script.py
@@ -1,3 +1,21 @@
 import pandas as pd
+from rich.console import Console
+from rich.prompt import IntPrompt
 
+console = Console()
 
+# read csv file
+survey_df = pd.read_csv(
+    '16 questions.csv',
+    delimiter=';')
+
+# define allowed choices
+allowed_choices = list(map(str, range(1,8)))
+
+console.print("\nTEST: Which type of programmer are you?\n")
+
+# print questions one by one and take answers
+for i in range(len(survey_df)):
+    console.print(f"[italic purple]Question {str(i+1)}: [/]")
+    console.print(survey_df.loc[i,'Question'])
+    answer = IntPrompt.ask("[italic]I do not agree (1) - I fully agree (7)[/]", choices=allowed_choices, show_choices=False)
